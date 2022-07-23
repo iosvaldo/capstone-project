391673d8a4c6aa1c0c4aad47c92c6f07ace71808
osvaldo <iosvaldo.app@gmail.com>
2022-07-22 22:26:58 -0400

    rebase file from messagesController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,19 @@ class UsersController < ApplicationController
   def create
     user = User.create!(user_params)
     render json: user
+    if user
+      room = Room.creaye(name: user.username, description: "This is the beginning of your new room")
+      user_rooms = UserRooms.create(user_id: user.id, room_id: room.id)
+      message = Message.create(body: "THis is the beginning of your new message", room_id: room.id, user_id: user.id)
+      # payload = {'user_id': user.id}
+      # token = encode(payload)
+      # render json: {
+      #   user: UserSerializer.new(user),
+      #   token: token,
+      #   authenticated: true
+      # }
+    else
+      render json: {message: "the system ran into an error when creating your account"}
   end
 
   def show
--- app/models/user_room.rb
+++ app/models/user_room.rb
@@ -1,4 +1,14 @@
 class UserRoom < ApplicationRecord
   belongs_to :user
   belongs_to :room
+
+  def create
+    user = User.find_by(username: params[:username])
+    user_rooms = UserRoom.create(user_id: user.id, room_id: params[:room_id])
+    room = Room.find(params[:room_id])
+    if room
+      puts 'Messave Saved!'
+      Room
+    end
+  end
 end
