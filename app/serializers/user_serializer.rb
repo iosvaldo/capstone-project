b9c38138039f6652de246fd5719aa869a18a0a56
osvaldo <iosvaldo.app@gmail.com>
2022-07-24 19:39:52 -0400

    hashed out code in user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :username, :password_digest, :active
-  attribute :rooms do |user|
-    user.rooms.uniq
-  end
+  # attribute :rooms do |user|
+  #   user.rooms.uniq
+  # end
+
+  # Lines 4,5,6 interfear with signing in. I keep getting this error whenever i do...
+  # ||||||||||| ERROR  BELOW ||||||
+  # app/serializers/user_serializer.rb:5:in `block in <class:UserSerializer>'
+  # app/controllers/sessions_controller.rb:8:in `create'
 end
