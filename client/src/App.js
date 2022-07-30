febd7210f678c85802cab64826122553d5b449b6
osvaldo <iosvaldo.app@gmail.com>
2022-07-30 14:12:22 -0400

    styled homepage 🐁

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-// import "./App.css"
+import "./App.css";
+import "./css/Home.css";
 import Signup from "./Signup"
 import Signin from "./Signin"
 import Home from "./Home"
--- client/src/Home.js
+++ client/src/Home.js
@@ -5,46 +5,69 @@ import { FaReact } from "react-icons/fa";
 import { SiRubyonrails } from "react-icons/si";
 import { IoIosPeople } from "react-icons/io";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container } from "react-bootstrap";
-// import "./App.css"
+import { FaRestroom } from "react-icons/fa";
+import { Accordion } from '@mantine/core';
+// import "./css/Home.css;"
+import "./App.css";
+
+
+//CSS Class Names on this page : home-title : chat-room-links 
+
 function Home() {
   return (
-    <Container
-      style={{
-        marginTop: "5%",
-        borderStyle: "solid",
-        borderColor: "grey"
-      }}
+    <Accordion
+      variant="contained"
+      radius="md"
+      chevronSize={30}
+      defaultValue="customization"
     >
-      <h1>What Chatroom are you interested in?</h1>
-      <br />
-      <ul className="app-list">
-        <li>
-          {" "}
-          <NavLink exact to="/rooms/1" className="app-items">
-            <FaReact /> Front End
+      <h1 className="home-title">
+        What Chatroom are you interested in? <FaRestroom></FaRestroom>
+      </h1>
+      <Accordion.Item value="customization">
+        <Accordion.Control>Front-End</Accordion.Control>
+        <Accordion.Panel>
+          Colors, fonts, shadows and many other parts are customizable to fit
+          your design needs{" "}
+          <NavLink exact to="/rooms/1" className="home-room-links">
+            <FaReact /> Front End{" "}
           </NavLink>{" "}
-        </li>
-        <li>
-          {" "}
-          <NavLink exact to="/rooms/2" className="app-items">
-            <SiRubyonrails /> Back End
+        </Accordion.Panel>
+      </Accordion.Item>
+
+      <Accordion.Item value="flexibility">
+        <Accordion.Control>Back End</Accordion.Control>
+        <Accordion.Panel>
+          Configure components appearance and behavior with vast amount of
+          settings or overwrite any part of component styles{" "}
+          <NavLink exact to="/rooms/2" className="home-room-links">
+            <SiRubyonrails /> Back End{" "}
           </NavLink>
-        </li>
-        <li>
-          {" "}
-          <NavLink exact to="/rooms/3" className="app-items">
-            <VscDebugConsole /> Debugging
-          </NavLink>
-        </li>
-        <li>
-          {" "}
-          <NavLink exact to="/rooms/4" className="app-items">
-            <IoIosPeople /> General
+        </Accordion.Panel>
+      </Accordion.Item>
+
+      <Accordion.Item value="focus-ring">
+        <Accordion.Control>General</Accordion.Control>
+        <Accordion.Panel>
+          With new :focus-visible pseudo-class focus ring appears only when user
+          navigates with keyboard{" "}
+          <NavLink exact to="/rooms/4" className="home-room-links">
+            <IoIosPeople /> General{" "}
           </NavLink>{" "}
-        </li>
-      </ul>
-    </Container>
+        </Accordion.Panel>
+      </Accordion.Item>
+
+      <Accordion.Item value="final">
+        <Accordion.Control>Debugging</Accordion.Control>
+        <Accordion.Panel>
+          With new :focus-visible pseudo-class focus ring appears only when user
+          navigates with keyboard{" "}
+          <NavLink exact to="/rooms/3" className="home-room-links">
+            <VscDebugConsole /> Debugging{" "}
+          </NavLink>
+        </Accordion.Panel>
+      </Accordion.Item>
+    </Accordion>
   );
 }
 
